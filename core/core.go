32e7d1bf8db1dd26d20dede73d4bb7a27f769345
Saurabh Mittal <saurabhmittal1606@gmail.com>
2024-01-27 02:27:14 +0530

    Add web server for node

--- core/core.go
+++ core/core.go
@@ -1,6 +1,13 @@
 package core
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 type key struct {
 	title string
@@ -13,18 +20,36 @@ type value struct {
 type Node struct {
 	id    uuid.UUID
 	elems map[key]value
+	port  int
+}
+
+func (node Node) Start() (*gin.Engine, error) {
+	router := gin.New()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": node.id,
+		})
+	})
+	address := fmt.Sprintf(":%d", node.port)
+	router.Run(address)
+	log.Print(fmt.Sprintf("Running node %d at port: %d", node.id, node.port))
+	return router, nil
 }
 
 func (node Node) Get(_key string) any {
 	val := node.elems[key{_key}]
 	return val.value
 }
-
 func (node Node) Put(_key string, _value any) {
 	node.elems[key{_key}] = value{value: _value}
 }
 
 func CreateNode() *Node {
-	node := Node{uuid.New(), map[key]value{}}
-	return &node
+	portNumber, err := GetAvailablePort()
+	if err == nil {
+		node := Node{uuid.New(), map[key]value{}, portNumber}
+		return &node
+	}
+	return nil
 }
---
+++ core/util.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+const START_RANGE int = 8000
+const END_RANGE int = 9000
+
+func GetAvailablePort() (int, error) {
+	for port := START_RANGE; port <= END_RANGE; port++ {
+		address := fmt.Sprintf(":%d", port)
+		listener, err := net.Listen("tcp", address)
+		if err == nil {
+			listener.Close()
+			log.Print(fmt.Sprintf("Found a port: %d", port))
+			return port, nil
+		}
+	}
+	return 0, fmt.Errorf("No available port!")
+}
