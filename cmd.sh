61e2afde64cca529c4be552102f0a24373aa854b
Saurabh Mittal <saurabhmittal1606@gmail.com>
2024-04-02 02:59:44 +0530

    Add generalised method to bash util

--- cmd.sh
+++ cmd.sh
@@ -15,12 +15,21 @@ genServiceRPC() {
     service/service.proto
 }
 
+genRPC() {
+    protoc \
+    --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    $1
+}
+
 # Check the argument passed and call the corresponding method
 if [ "$1" = "genPlaygroundRPC" ]; then
     genPlaygroundRPC
 elif [ "$1" = "genServiceRPC" ]; then
     genServiceRPC
+elif [ "$1" = "genRPC" ]; then
+    genRPC $2
 else
-    echo "Usage: $0 [genPlaygroundRPC|genServiceRPC]"
+    echo "Usage: $0 [genPlaygroundRPC | genServiceRPC | genRPC <.proto>]"
     exit 1
 fi
