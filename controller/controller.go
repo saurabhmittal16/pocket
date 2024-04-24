e268eb1565348fb963a75934741daca27bdae0fe
Saurabh Mittal <saurabhmittal1606@gmail.com>
2024-04-24 23:43:17 +0530

    Add consistent hashing node insert

--- controller/controller.go
+++ controller/controller.go
@@ -1,21 +1,29 @@
 package controller
 
 import (
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
 	"log"
+	"sort"
 	"sync"
 
 	"github.com/saurabhmittal16/pocket/worker"
 )
 
+// total slots for hash space
+const TOTAL_SLOTS = 1e10
+
 var lock = &sync.Mutex{}
 
 type workerNode struct {
-	id   int
-	port int
+	Id   string
+	Port int
+	Hash uint64
 }
 
 type controllerNode struct {
-	workers []workerNode
+	ring []workerNode
 }
 
 var instance *controllerNode
@@ -25,13 +33,14 @@ func GetInstance() *controllerNode {
 		lock.Lock()
 		defer lock.Unlock()
 
-		instance = &controllerNode{workers: []workerNode{}}
+		// workers[i] is at space[i] in the hash space
+		instance = &controllerNode{ring: []workerNode{}}
 	}
 	return instance
 }
 
 func (c *controllerNode) CreateWorkers(numWorkers int) error {
-	count := len(c.workers)
+	count := len(c.ring)
 	ports, err := GetAvailablePorts(numWorkers)
 
 	if err != nil {
@@ -42,16 +51,39 @@ func (c *controllerNode) CreateWorkers(numWorkers int) error {
 		port := ports[i]
 
 		// create workerNode instance
-		id := count + i
-		w := workerNode{id: id, port: port}
+		id := fmt.Sprintf("Worker#%05d", count+i+1)
+		w := workerNode{Id: id, Port: port, Hash: hash(id)}
 
 		// spin up REST server for worker node
 		go worker.SpinWorker(port)
 
 		// save the workerNode instance
-		c.workers = append(c.workers, w)
+		c.addToRing(w)
 
-		log.Printf("Successfuly started worker node (Id = %d) at %d", id, port)
+		// TODO: Get error from SpinWorker and log accordingly
+		log.Printf("Successfuly started worker node (Id = %s) at %d", id, port)
 	}
 	return nil
+}
+
+func (c *controllerNode) addToRing(w workerNode) {
+	c.ring = append(c.ring, w)
+	sort.SliceStable(c.ring, func(i, j int) bool {
+		return c.ring[i].Hash < c.ring[j].Hash
+	})
+}
+
+func (c *controllerNode) GetRing() []workerNode {
+	return c.ring
+}
+
+func hash(key string) uint64 {
+	// generate the hash of key
+	sum := sha256.Sum256([]byte(key))
+
+	// use the first 32 bytes to generate integer position
+	data := binary.BigEndian.Uint64(sum[:32])
+
+	// take mod with TOTAL SLOTS space
+	return data % TOTAL_SLOTS
 }
